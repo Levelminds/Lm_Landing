e7154525f6cb5e2fb52fe5e134a5a1ca972a45c8
Levelminds <dev@levelminds.in>
2025-10-11 20:48:22 +0530

    Update blogs.php

--- blogs.php
+++ blogs.php
@@ -1,7 +1,7 @@
 <?php
-error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $host = 'localhost';
 $dbname = 'u420143207_LM_landing';
 $username = 'u420143207_lmlanding';
